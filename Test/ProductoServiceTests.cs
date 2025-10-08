433a3d8d8921139a9467e4d470b09d76ca8595ef
JonProg88 <alanisJon_Pro@outlook.com>
2025-10-07 23:33:41 -0600

    Agregado modulo de pruebas ESE SI Contiene unitarias con xUnit y Moq

---
+++ Test/ProductoServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using ProductosAPI.Models;
+using ProductosAPI.Repositories;
+using ProductosAPI.Services;
+using Xunit;
+
+namespace ProductosAPI.Test
+{
+    /// <summary>
+    /// Prueba unitaria simple para validar que se cumpla el flujo de creacion de producto
+    /// </summary>
+    public class ProductoServiceTests
+    {
+        [Fact]
+        public async Task CrearAsync_DeberiaLlamarAlRepositorio()
+        {
+            // Arrange
+            var mockRepo = new Mock<IProductoRepository>();
+            var service = new ProductoService(mockRepo.Object);
+
+            var producto = new Producto { Id = 1, Nombre = "Laptop", Precio = 25000, Stock = 5 };
+
+            // Act
+            await service.CrearAsync(producto);
+
+            // Assert
+            mockRepo.Verify(r => r.CrearAsync(It.IsAny<Producto>()), Times.Once);
+
+        }
+
+
+    }
+}
